14b60f410ad1f07a549df58d8bbd7c58ee4d878c
sylvexia <cuberobin@gmail.com>
2024-12-24 15:18:43 +0800

    added differnt config of posit

--- positWrapperPybind.cpp
+++ positWrapperPybind.cpp
@@ -19,9 +19,22 @@ void bind_posit_functions(py::module_ &m, const std::string &suffix) {
           py::arg("d"));
 }
 
+// libpositWrapperPy matches the name of the .so file
 PYBIND11_MODULE(libpositWrapperPy, m) {
     bind_posit_functions<8, 0, uint8_t>(m, "8_0");
+    bind_posit_functions<8, 1, uint8_t>(m, "8_1");
+    bind_posit_functions<8, 2, uint8_t>(m, "8_2");
+    bind_posit_functions<8, 3, uint8_t>(m, "8_3");
+    bind_posit_functions<16, 0, uint16_t>(m, "16_0");
     bind_posit_functions<16, 1, uint16_t>(m, "16_1");
+    bind_posit_functions<16, 2, uint16_t>(m, "16_2");
+    bind_posit_functions<16, 3, uint16_t>(m, "16_3");
+    bind_posit_functions<32, 0, uint32_t>(m, "32_0");
+    bind_posit_functions<32, 1, uint32_t>(m, "32_1");
     bind_posit_functions<32, 2, uint32_t>(m, "32_2");
+    bind_posit_functions<32, 3, uint32_t>(m, "32_3");
+    bind_posit_functions<64, 0, uint64_t>(m, "64_0");
+    bind_posit_functions<64, 1, uint64_t>(m, "64_1");
+    bind_posit_functions<64, 2, uint64_t>(m, "64_2");
     bind_posit_functions<64, 3, uint64_t>(m, "64_3");
 }
--- test.py
+++ test.py
@@ -1,7 +1,33 @@
 import libpositWrapperPy as posit
 
-rawBit = posit.getRawBit_32_2(1.79769e+12)
-print(rawBit)
+print("posit(64, 0)")
 
-doubleVal = posit.getDouble_32_2(rawBit)
+rawBit = posit.getRawBit_64_0(1e+307)
+print(bin(rawBit))
+
+doubleVal = posit.getDouble_64_0(rawBit)
+print(doubleVal)
+
+print("posit(64, 1)")
+
+rawBit = posit.getRawBit_64_1(1e+307)
+print(bin(rawBit))
+
+doubleVal = posit.getDouble_64_1(rawBit)
+print(doubleVal)
+
+print("posit(64, 2)")
+
+rawBit = posit.getRawBit_64_2(1e+307)
+print(bin(rawBit))
+
+doubleVal = posit.getDouble_64_2(rawBit)
+print(doubleVal)
+
+print("posit(64, 3)")
+
+rawBit = posit.getRawBit_64_3(1e+307)
+print(bin(rawBit))
+
+doubleVal = posit.getDouble_64_3(rawBit)
 print(doubleVal)
